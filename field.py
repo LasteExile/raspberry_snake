3391b205e5f46f1bb026d9e34321817d8425525c
lasteexile <bbriz020903@gmail.com>
2020-01-31 20:30:22 +0200

    Added food coord

--- field.py
+++ field.py
@@ -1,4 +1,4 @@
-def get_field(background_color, snake_color, xy):
+def get_field(background_color, snake_color, food_color, xy, food = None):
     game_field = [
         [background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color, background_color],
 
@@ -35,4 +35,7 @@ def get_field(background_color, snake_color, xy):
     
     for i in xy:
         game_field[i[0]][i[1]] = snake_color
+    if food != None:
+        game_field[food[0]][food[1]] = food_color
+
     return game_field
